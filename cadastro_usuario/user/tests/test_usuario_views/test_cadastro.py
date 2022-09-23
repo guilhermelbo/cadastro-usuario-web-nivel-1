75cd730845b9dbd31c133092531f68268297ca0a
Guilherme Luiz Borges de Oliveira <gui12051@gmail.com>
2022-09-22 22:16:38 -0300

    Update tests

--- cadastro_usuario/user/tests/test_usuario_views/test_cadastro.py
+++ cadastro_usuario/user/tests/test_usuario_views/test_cadastro.py
@@ -1,8 +1,48 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from django.urls import reverse, resolve
 from user.views import cadastro
 
 class CadastroTest(TestCase):
+    def cliente_logado(self):
+        client = Client()
+        data = {
+            'nome': 'a',
+            'email': 'a@a',
+            'cpf': '11111111111',
+            'pis': '11111111111',
+            'pais': 'a',
+            'estado': 'a',
+            'municipio': 'a',
+            'cep': '11111111',
+            'rua': 'a',
+            'numero': '1',
+            'complemento': 'a',
+            'senha': 'a',
+        }
+        client.post(reverse('valida_cadastro'), data)
+        client.post(reverse('valida_login'), {'login': 'a@a', 'senha':'a'})
+        return client
+
     def test_cadastro_view_function_is_correct(self):
         view = resolve(reverse('cadastro'))
         self.assertIs(view.func, cadastro)
+    
+    def test_cadastro_view_returns_status_code_200(self):
+        response = self.client.get(reverse('cadastro'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_cadastro_view_loads_correct_template(self):
+        response = self.client.get(reverse('cadastro'))
+        self.assertTemplateUsed(response, 'user/cadastro.html')
+    
+    def test_cadastro_view_redirect_correct_template_when_is_logged(self):
+        client = self.cliente_logado()
+        response = client.get(reverse('cadastro'))
+        self.assertRedirects(response, reverse('home'))
+    
+    def test_cadastro_view_content_when_user_not_logged(self):
+        response = self.client.get(reverse('cadastro'))
+        self.assertIn('<title>Cadastro</title>', response.content.decode('utf-8'))
+        self.assertIn('Cadastro', response.content.decode('utf-8'))
+        self.assertIn('Nome', response.content.decode('utf-8'))
+        self.assertIn('Email', response.content.decode('utf-8'))
--- cadastro_usuario/user/tests/test_usuario_views/test_valida_cadastro.py
+++ cadastro_usuario/user/tests/test_usuario_views/test_valida_cadastro.py
@@ -1,8 +1,83 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from django.urls import reverse, resolve
 from user.views import valida_cadastro
+from user.models import Usuario, Endereco
+from hashlib import sha256
 
 class ValidaCadastroTest(TestCase):
     def test_valida_cadastro_view_function_is_correct(self):
         view = resolve(reverse('valida_cadastro'))
         self.assertIs(view.func, valida_cadastro)
+    
+    def test_valida_cadastro_redirect_when_any_field_is_empty(self):
+        response = self.client.get(reverse('valida_cadastro'))
+        self.assertRedirects(response, reverse('cadastro'))
+    
+    def test_valida_cadastro_when_email_is_used_for_another_user(self):
+        senha = sha256('a'.encode()).hexdigest()
+        endereco = Endereco.objects.create(pais='a', estado='a', municipio='a', cep='11111111', rua='a', numero='1', complemento='a')
+        Usuario.objects.create(nome='a', email='a@a', cpf='11111111111', pis='11111111111', senha=senha, endereco_id=endereco)
+
+        data = {
+            'nome': 'a',
+            'email': 'a@a',
+            'cpf': '11111111111',
+            'pis': '11111111111',
+            'pais': 'a',
+            'estado': 'a',
+            'municipio': 'a',
+            'cep': '11111111',
+            'rua': 'a',
+            'numero': '1',
+            'complemento': 'a',
+            'senha': senha,
+        }
+        response = self.client.post(reverse('valida_cadastro'), data, follow=True)
+        self.assertIn('O email já está sendo utilizado por outro usuário.', response.content.decode('utf-8'))
+        self.assertRedirects(response, reverse('cadastro'))
+    
+    def test_valida_cadastro_when_cpf_is_used_for_another_user(self):
+        senha = sha256('a'.encode()).hexdigest()
+        endereco = Endereco.objects.create(pais='a', estado='a', municipio='a', cep='11111111', rua='a', numero='1', complemento='a')
+        Usuario.objects.create(nome='a', email='a@a', cpf='11111111111', pis='11111111111', senha=senha, endereco_id=endereco)
+
+        data = {
+            'nome': 'a',
+            'email': 'b@b',
+            'cpf': '11111111111',
+            'pis': '11111111111',
+            'pais': 'a',
+            'estado': 'a',
+            'municipio': 'a',
+            'cep': '11111111',
+            'rua': 'a',
+            'numero': '1',
+            'complemento': 'a',
+            'senha': senha,
+        }
+        response = self.client.post(reverse('valida_cadastro'), data, follow=True)
+        self.assertIn('O cpf já está sendo utilizado por outro usuário.', response.content.decode('utf-8'))
+        self.assertRedirects(response, reverse('cadastro'))
+    
+    def test_valida_cadastro_when_pis_is_used_for_another_user(self):
+        senha = sha256('a'.encode()).hexdigest()
+        endereco = Endereco.objects.create(pais='a', estado='a', municipio='a', cep='11111111', rua='a', numero='1', complemento='a')
+        Usuario.objects.create(nome='a', email='a@a', cpf='11111111111', pis='22222222222', senha=senha, endereco_id=endereco)
+
+        data = {
+            'nome': 'a',
+            'email': 'b@b',
+            'cpf': '22222222222',
+            'pis': '22222222222',
+            'pais': 'a',
+            'estado': 'a',
+            'municipio': 'a',
+            'cep': '11111111',
+            'rua': 'a',
+            'numero': '1',
+            'complemento': 'a',
+            'senha': senha,
+        }
+        response = self.client.post(reverse('valida_cadastro'), data, follow=True)
+        self.assertIn('O pis já está sendo utilizado por outro usuário.', response.content.decode('utf-8'))
+        self.assertRedirects(response, reverse('cadastro'))
